7b042e91c68e7c43fc2098700bb57a517b5a235c
onlinekkabchi <onlinekkabchi@gmail.com>
2022-07-17 18:30:13 +0900

    refactor: 검색창 열려있을 경우 무한스크롤안되게 수정

--- hosting/files/infinite-scroll.js
+++ hosting/files/infinite-scroll.js
@@ -1,10 +1,11 @@
 import { drawForInfiniteScroll } from "./product-card-draw.js";
+import { cutequery } from "./make-cutie-query.js";
 
 export default function infiniteScroll() {
     const main = document.querySelector("main");
     const fullHeight = main.scrollHeight;
     const userPositionHeight = window.scrollY;
-    if (fullHeight - userPositionHeight < 1000) {
+    if (!cutequery(".search-page") && fullHeight - userPositionHeight < 1000) {
         drawForInfiniteScroll();
     }
 }
